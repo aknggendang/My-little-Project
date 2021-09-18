1ae36a309d475b7e6d65dbdce3ffba111bebb213
A.Ahmad Zahran <73604955+aknggendang@users.noreply.github.com>
2021-09-18 19:56:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var a=prompt("Masukin Nama Panggilan");
+var b=prompt("Masukin Tahun lahir");
+var c=prompt("Yakin Mau lanjut?")
+  	if(a== "Nadine" && b=="2004" && c=="Yakin")
+  	{
+  	    window.location.href="birthday.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
